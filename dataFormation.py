1d165f27afef498aed4f3b0843050f8434dd6a70
Shreyas K N <shreyas2499@gmail.com>
2023-12-25 12:31:07 -0500

    Dataset links added

--- dataFormation.py
+++ dataFormation.py
@@ -1,15 +1,40 @@
 # Search Dataset used:  https://www.kaggle.com/code/tataiee1375/starter-question-pairs-dataset-6135b053-9
+#                       https://www.kaggle.com/datasets/imoore/60k-stack-overflow-questions-with-quality-rate
+#                       https://rajpurkar.github.io/SQuAD-explorer/
+
 
 import pandas as pd
+import json
 
-
-def extract_questions():
+# Function to extract questions from Q&A dataset
+def extract_from_qna():
+    # Read Q&A dataset from CSV
     df = pd.read_csv("questions.csv")
     questions = df['question2']
     with open("output.txt", 'a', encoding='utf-8') as file:
         for row in questions:
             file.write(f"{row}\n")
 
-extract_questions()
+# Function to extract questions from Stack Overflow dataset
+def extract_from_stack_overflow():
+    df = pd.read_csv("train.csv")
+    questions = df['Title']
+    with open("output.txt", 'a', encoding='utf-8') as file:
+        for row in questions:
+            file.write(f"{row}\n")
+
+# Function to extract questions from SQuAD dataset
+def extract_from_squad():
+    with open("output.txt", 'a', encoding='utf-8') as file:
+        with open('train.json') as f:
+            data = json.load(f)
+            for rec in data['data']:
+                for ques in rec["paragraphs"]:
+                    for qas in ques['qas']:
+                        file.write(f"{qas['question']}\n")
 
 
+if __name__ == "__main__":
+    extract_from_qna()
+    extract_from_stack_overflow()
+    extract_from_squad()
--- snippet.py
+++ snippet.py
@@ -1,61 +1,55 @@
+# Import necessary libraries
 import openai
 from openai import OpenAI
-from googlesearch import search
 from bs4 import BeautifulSoup
 import requests
 from urllib import robotparser
 import pandas as pd
 import random
 import time
+from secretKey import openAIKey
 
-promptList2 = ["Generate a snippet from the below data in approximately ", "Provide a summary of the below content in approximately "]
+# Define a list of prompts for GPT-3.5-turbo
+promptList = ["Summarize the main idea and key points of the paragraph about: topic_content.",
+              "Provide a concise summary of the paragraph discussing ' topic_content ' in detail.",
+              "Condense the information in the paragraph regarding ' topic_content ' into a brief overview.",
+              "Give me a short summary highlighting the crucial information in the paragraph about: topic_content.",
+              "Create a snippet summarizing the paragraph's key insights on ' topic_content ' and its implications."]
 
-promptList = ["Summarize the main idea and key points of the paragraph about: topic_content", "Provide a concise summary of the paragraph discussing ' topic_content ' in detail.",
-"Condense the information in the paragraph regarding ' topic_content ' into a brief overview.", "Give me a short summary highlighting the crucial information in the paragraph about: topic_content.",
-"Create a snippet summarizing the paragraph's key insights on ' topic_content ' and its implications."]
+# Define headers for the generated snippets
+promptHeaders = ["chatGpt1", "chatGpt2", "chatGpt3", "chatGpt4", "chatGpt5"]
 
+# Set the OpenAI API key
+openai.api_key = openAIKey
 
-columns = ["a", "b", "c"]
-openai.api_key = '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'
-
-
-
-def get_results(query):
-    urlList = []
-
-    for i in search(query=query, tld="com", num=10, stop=10, pause=2):
-        urlList.append(i)
-    
-    return urlList
-
-
+# Function to fetch Google snippets for a given query
 def get_google_snippets(query):
-    googleSnippets = {}
-    headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
-    url=f'https://www.google.com/search?q={query}&ie=utf-8&oe=utf-8&num=10'
-    html = requests.get(url,headers=headers)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
+    url = f'https://www.google.com/search?q={query}&ie=utf-8&oe=utf-8&num=10'
+    html = requests.get(url, headers=headers)
 
-    if(html.status_code == 200):
+    if html.status_code == 200:
         soup = BeautifulSoup(html.text, 'html.parser')
         counter = 0
-        
         summary = []
-        topStuff = soup.find('div', {"id": "search"})
-        bottomStuff = soup.find('div', {"id": "botstuff"})
 
+        # Extract relevant information from Google search results
         for container in soup.findAll('div', class_="tF2Cxc"):
-            try: 
+            try:
                 heading = container.find('h3', class_='LC20lb').text
                 article_summary = container.find('div', class_='VwiC3b').text
-                link = container.find('a')['href']    
-                if("youtube" in link.lower()):
+                link = container.find('a')['href']
+
+                # Skip YouTube links
+                if "youtube" in link.lower():
                     continue
 
                 print(heading, ": ", article_summary, "\n\n")
             except Exception as e:
-                continue    
+                continue
             counter = counter + 1
-            
+
+            # Append information to the summary list
             summary.append({
                 'heading': heading,
                 'snippet': article_summary,
@@ -64,7 +58,7 @@ def get_google_snippets(query):
 
     return summary
 
-
+# Function to parse the content of a URL and return the body text
 def parse_url(url):
     # Return ONLY Body. Everything else needs to be done here
     str = ""
@@ -79,102 +73,104 @@ def parse_url(url):
             response = requests.get(url, timeout=10)
 
             if response.status_code == 200:
-                
                 soup = BeautifulSoup(response.text, 'html.parser')
 
+                # Get the body content and clean it
                 body_content = soup.body.get_text()
-
                 body_content_cleaned = body_content.replace('\n', '')
-
                 str = body_content_cleaned
 
-        # response = requests.get(url)
-    except Exception as e:  
+    except Exception as e:
         return str
-   
+
     return str
 
-
+# Function to create a prompt for GPT-3.5-turbo based on the query and selected prompt
 def get_prompt(prompt, query):
     pL = prompt.split("topic_content")
-
     newPrompt = pL[0]
     newPrompt = newPrompt + query
-    if(len(pL)>2):
+    if len(pL) > 2:
         newPrompt = newPrompt + pL[2]
 
     return newPrompt
 
-
-def get_snippet_from_gpt(updatedGoogleSnippet, query):
-    # print(promptList)
-
-    # print("saoddna")    
+# Function to generate snippets using GPT-3.5-turbo
+def get_snippet_from_gpt(updatedGoogleSnippet):
     for val in updatedGoogleSnippet:
         for index, prompt in enumerate(promptList):
-            
+            if len(val) == 0:
+                continue
             time.sleep(25)
-            # message = input("User: ")
-            client = OpenAI(
-            
-            api_key=openai.api_key,
-            )
 
-            chat_completion = client.chat.completions.create(
-                messages=[
-                    { "role": "system", "content": get_prompt(prompt, query) + "Also do so in " + str(len(val['snippet'])) + " characters: "},
-                    { "role": "user", "content": val['body']}
-                ],
-                model="gpt-3.5-turbo",
-            )
+            try:
+                client = OpenAI(api_key=openai.api_key,)
 
-            val[index] = chat_completion.choices[0].message.content
-            print(chat_completion.choices[0].message.content, "\n")
+                # Create a chat completion using GPT-3.5-turbo
+                chat_completion = client.chat.completions.create(
+                    messages=[
+                        {"role": "system",
+                         "content": get_prompt(prompt, val['query']) + "Also do so in about" + str(
+                             len(val['snippet']) + 100) + " characters:"},
+                        {"role": "user", "content": val['body']}
+                    ],
+                    model="gpt-3.5-turbo",
+                )
+
+                # Update the snippet in the dictionary with the generated content
+                val[promptHeaders[index]] = chat_completion.choices[0].message.content
+            except Exception as e:
+                val[promptHeaders[index]] = ""
 
     return updatedGoogleSnippet
-            
 
-
+# Function to read and pick a random question from the file "output.txt"
 def read_and_pick_question():
     with open("output.txt", 'r', encoding='utf-8') as file:
         questions = file.readlines()
 
-    
     questions = [question.strip() for question in questions]
 
-    
     random_question = random.choice(questions)
 
     return random_question
 
+
+
 if __name__ == "__main__":
-    query = read_and_pick_question()
-    print(query, "\n\n\n\n")
-    # query = "what is encapsulation"
-    # topic = query
-
-    
-    
-    googleSnippets = get_google_snippets(query)
+    start_time = time.time()
     updatedGoogleSnippet = []
-    for val in googleSnippets:
-        body = parse_url(val['url'])
-        val['body'] = body
-        val["query"] = query
-        if(body != ""):
-            updatedGoogleSnippet.append(val)
 
+    while len(updatedGoogleSnippet) < 20:
+        query = read_and_pick_question()
 
-    updatedGoogleSnippet = updatedGoogleSnippet[0]
-    
-    print("something")
+        # Fetch Google snippets for the query
+        googleSnippets = get_google_snippets(query)
+        tempSnippets = []
 
-    updatedGoogleSnippet = get_snippet_from_gpt([updatedGoogleSnippet], query)
-    
-    df = pd.DataFrame(updatedGoogleSnippet, columns=columns)
+        # Parse URLs and prepare snippets for GPT-3.5-turbo
+        for val in googleSnippets:
+            body = parse_url(val['url'])
+            val['body'] = body[:4000]
+            val["query"] = query
+            if body != "":
+                tempSnippets.append(val)
+
+        # Append selected snippets to the list
+        for snip in tempSnippets[:3]:
+            updatedGoogleSnippet.append(snip)
+
+    # Generate snippets using GPT-3.5-turbo
+    updatedGoogleSnippet = get_snippet_from_gpt(updatedGoogleSnippet)
+
+    # Create a DataFrame and save it to an Excel file
+    df = pd.DataFrame(updatedGoogleSnippet)
     excel_file = 'snippet_files.xlsx'
     df.to_excel(excel_file, index=False)
 
-    print("something2")
+    end_time = time.time()
 
+    # Calculate the elapsed time
+    elapsed_time = end_time - start_time
 
+    print("Done: ", elapsed_time)
